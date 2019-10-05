a226d989f4da65c5c00ae2456c571ec344b69e95
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-05 20:26:55 +0100

    Generate devise controllers for users and write routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers : {
+    sessions: 'users/sessions'
+    confirmations: 'users/confirmations'
+    passwords: 'users/passwords'
+    unlocks: 'users/unlocks'
+    omniauth_callbacks: 'users/omniauth_callbacks'
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
