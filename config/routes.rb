5beca7b3a11e020ea52c30b87919ae4f2ed94060
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-06 02:09:25 +0100

    Write routes and install webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers : {
-    sessions: 'users/sessions'
-    confirmations: 'users/confirmations'
-    passwords: 'users/passwords'
-    unlocks: 'users/unlocks'
-    omniauth_callbacks: 'users/omniauth_callbacks'
+  root 'users#timeline'
+  get '/dashboard', to: 'users#show'
+  
+  resources :posts
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    unlocks: 'users/unlocks',
+    omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
