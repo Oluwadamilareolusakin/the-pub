ba60178927271e59b0fd364debd2a4fd3f55ade3
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-06 02:08:57 +0100

    Set up user model associations and validations, remove devise views from devise directory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
+  has_many :posts
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, :trackable
