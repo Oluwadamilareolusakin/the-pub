91567b7b38c9f482277bb8f5eb0d22f78442bb68
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-09 19:19:41 +0100

    Add comments and likes controller actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[ destroy ]
+  before_action :authenticate_user!
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      flash[:success] = "Eyy, nice comment!"
+      redirect_back_or_to root_path
+    else
+      flash[:notice] = "Something seems to be wrong with your comment"
+      redirect_back_or_to root_path
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    flash[:success] = "Your comment was deleted"
+    redirect_back_or_to root_path
+  end
+
+  private
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+end
