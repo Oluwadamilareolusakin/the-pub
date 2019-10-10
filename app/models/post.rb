96cb694a36a6943d7b980f6572001ad1b5e328ad
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-10 17:44:41 +0100

    Add dependent destroy on has many associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@
 class Post < ApplicationRecord
   default_scope { order(created_at: :desc) }
   belongs_to :user
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   validates :content, presence: true
-  has_many :likes, as: :likeable
+  has_many :likes, as: :likeable, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: true
-  has_many :posts
-  has_many :comments
-  has_many :likes
-  has_many :images
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :images, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, :trackable
