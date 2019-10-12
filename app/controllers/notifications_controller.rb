6bc8aa6a977176f9d5d169c430331f3ecb8552f2
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-12 15:53:27 +0100

    Restructure mark as read

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,10 +4,11 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
   def index
     @notifications = Notification.where(receipient: current_user).unread
- end
+  end
 
   def mark_as_read
     @notifications = Notification.where(receipient: current_user).unread
-    @notifications.update_all(:read_at, Time.zone.now)
+    @notifications.update_all(read_at: Time.zone.now)
+    render json: {success: true}
   end
 end
