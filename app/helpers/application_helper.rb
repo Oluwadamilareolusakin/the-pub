7fa8240736020f10133749e1044fae97a851dfdf
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-15 18:36:17 +0100

    Use guard clause in friendship button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,18 +6,16 @@ module ApplicationHelper
   end
 
   def friendship_button(user)
-    if not_same_user(user)
-      if current_user.friend_request_with?(user) && !current_user.friends_with(user) && !current_user.requester?(user)
-        link_to 'Accept Friend Request', friendships_path(id: user), method: :post, class: 'friendship-btn'
+    return if !not_same_user(user)
+    if current_user.friend_request_with?(user) && !current_user.friends_with(user) && !current_user.requester?(user)
+      link_to 'Accept Friend Request', friendships_path(id: user), method: :post, class: 'friendship-btn'
 
-      elsif !current_user.friend_request_with?(user)
-        link_to 'Friend+', friend_requests_path(id: user), method: :post, class: 'friendship-btn'
+    elsif !current_user.friend_request_with?(user)
+      link_to 'Friend+', friend_requests_path(id: user), method: :post, class: 'friendship-btn'
 
-      elsif !current_user.friends_with(user) && current_user.requester?(user)
-        link_to 'Cancel Request', friend_request_path(id: user), method: :delete, class: 'cancel-friendship'
-      elsif current_user.friends_with(user)
-        link_to 'Unfriend', friendship_path(id: user), method: :delete, class: 'cancel-friendship'
-      end
+    elsif !current_user.friends_with(user) && current_user.requester?(user)
+      link_to 'Cancel Request', friend_request_path(id: user), method: :delete, class: 'cancel-friendship'
+    elsif current_user.friends_with(user)
+      link_to 'Unfriend', friendship_path(id: user), method: :delete, class: 'cancel-friendship'
     end
-  end
 end
