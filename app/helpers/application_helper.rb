f78b72b8c3df4f038b61c7c2bb9d910d47d15e20
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-17 20:33:30 +0100

    Revert friendship button logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,11 @@ module ApplicationHelper
     return unless not_same_user(user)
     if current_user.friend_request_with?(user) && !current_user.friends_with(user) && !current_user.requester?(user)
       link_to 'Accept Friend Request', friendships_path(id: user), method: :post, class: 'friendship-btn'
-    end
-    if !current_user.friend_request_with?(user)
+
+    elsif !current_user.friend_request_with?(user)
       link_to 'Friend+', friend_requests_path(id: user), method: :post, class: 'friendship-btn'
-    end
-    if !current_user.friends_with(user) && current_user.requester?(user)
+
+    elsif !current_user.friends_with(user) && current_user.requester?(user)
       link_to 'Cancel Request', friend_request_path(id: user), method: :delete, class: 'cancel-friendship'
     elsif current_user.friends_with(user)
       link_to 'Unfriend', friendship_path(id: user), method: :delete, class: 'cancel-friendship'
