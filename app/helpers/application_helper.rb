90b2f3de29acdd6e82f42265d07e693fc793a20a
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-17 13:09:57 +0100

    Refactor friendship button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,24 +7,19 @@ module ApplicationHelper
 
   def friendship_button(user)
     return unless not_same_user(user)
-
     if current_user.friend_request_with?(user) && !current_user.friends_with(user) && !current_user.requester?(user)
       link_to 'Accept Friend Request', friendships_path(id: user), method: :post, class: 'friendship-btn'
-
-    elsif !current_user.friend_request_with?(user)
+      return
+    end
+    if !current_user.friend_request_with?(user)
       link_to 'Friend+', friend_requests_path(id: user), method: :post, class: 'friendship-btn'
-
-    elsif !current_user.friends_with(user) && current_user.requester?(user)
+      return
+    end
+    
+    if !current_user.friends_with(user) && current_user.requester?(user)
       link_to 'Cancel Request', friend_request_path(id: user), method: :delete, class: 'cancel-friendship'
     elsif current_user.friends_with(user)
       link_to 'Unfriend', friendship_path(id: user), method: :delete, class: 'cancel-friendship'
     end
-  end
-
-  def pending_notifications
-    @notifications = Notification.where(receipient: current_user).unread
-    return if @notifications.any?
-
-    'No pending notifications'
   end
 end
