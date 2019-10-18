259286f78942a4062e783e79e852c631ebbb1daa
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-19 00:11:02 +0100

    Correct lint errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,7 @@ module ApplicationHelper
     return unless not_same_user(user)
 
     if current_user.friend_request_with?(user) && !current_user.friends_with(user) && !current_user.requester?(user)
-      return link_to 'Accept Request', friendships_path(id: user),
-                                                                 method: :post, class: 'friendship-btn relationship-btn'
+      return link_to 'Accept Request', friendships_path(id: user),method: :post, class: 'friendship-btn relationship-btn'
     end
 
     unless current_user.friend_request_with?(user)
@@ -18,8 +17,7 @@ module ApplicationHelper
     end
 
     unless current_user.friends_with(user) && current_user.requester?(user)
-      return link_to 'Cancel Request', friend_request_path(id: user),
-                                                                    method: :delete, class: 'cancel-friendship relationship-btn'
+      return link_to 'Cancel Request', friend_request_path(id: user), method: :delete, class: 'cancel-friendship relationship-btn'
     end
 
     return unless current_user.friends_with(user)
