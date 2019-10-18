66a0438708646063758328dd6d970f77a1851563
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-18 22:38:24 +0100

    Rename blank user image, add class to relationship button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,19 +9,19 @@ module ApplicationHelper
     return unless not_same_user(user)
 
     if current_user.friend_request_with?(user) && !current_user.friends_with(user) && !current_user.requester?(user)
-      link_to 'Accept Friend Request', friendships_path(id: user), method: :post, class: 'friendship-btn'
+      link_to 'Accept Friend Request', friendships_path(id: user), method: :post, class: 'friendship-btn relationship-btn'
     end
 
     unless current_user.friend_request_with?(user)
-      return link_to 'Friend+', friend_requests_path(id: user), method: :post, class: 'friendship-btn'
+      return link_to 'Friend+', friend_requests_path(id: user), method: :post, class: 'friendship-btn relationship-btn'
     end
 
     unless current_user.friends_with(user) && current_user.requester?(user)
-      return link_to 'Cancel Request', friend_request_path(id: user), method: :delete, class: 'cancel-friendship'
+      return link_to 'Cancel Request', friend_request_path(id: user), method: :delete, class: 'cancel-friendship relationship-btn'
     end
 
     if current_user.friends_with(user)
-      link_to 'Unfriend', friendship_path(id: user), method: :delete, class: 'cancel-friendship'
+      link_to 'Unfriend', friendship_path(id: user), method: :delete, class: 'cancel-friendship relationship-btn'
     end
   end
 
